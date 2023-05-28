e12856042eeb58d67ab85dc85b16082a0cc9a0db
Yi Lin <yiiiliiinn@gmail.com>
2023-05-29 01:47:36 +0800

    tried exam paper

--- SmartyPants/exampaper.py
+++ SmartyPants/exampaper.py
@@ -1,34 +1,62 @@
 import streamlit as st
-from toolkit import pdf_to_txt, split
+from toolkit import pdf_to_txt, split, prompt, generate_pdf
+from langchain import LLMChain
 from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from langchain.output_parsers import CommaSeparatedListOutputParser
 
-# Prompt Template
+warning = """
+his app generates exam papers with questions and answers, powered by GPT-3. You can download the questions as a PDF file. 
+However, please note that the accuracy of the generated questions may vary depending on the topic. 
+It is recommended to verify the information by searching online or consulting an expert.
 
-first_prompt = """ You are an esteemed teacher and a question setter. Given the file, for each question, identify the topic and type of question. 
-The type of questions are as follows:
-1. Multiple Choice Questions (MCQs):
-   a. Include MCQs with options.
-   b. Ensure a balanced distribution of difficulty levels.
-
-2. Short Answer Questions:
-   a. Provide short answer questions covering different topics.
-   b. Each question should require a concise answer within {word limit} words.
-
-3. Essay Questions:
-   a. Include essay questions that require in-depth explanations.
-   b. Each question should cover a specific aspect of the course material.
-
-4. Problem-solving Questions:
-   a. Design problem-solving questions that test application skills.
-   b. Ensure a mix of theoretical and practical scenarios.
-
-The topics are the central theme of the question. 
-
-Present the breakdown in the following manner: [(Question 1 topic, Question 1 type), (Question 2 topic, Question 2 type),...]
+Please keep in mind that this app is for educational purposes only. 
+It is designed to help students practice and test their knowledge, and it is not intended for creating exam papers in real-world scenarios.
 """
+def generate_html(exam_data):
+    html = "<html><body>"
+    html += "<h1>MCQ Exam Paper</h1>"
+    for index, question in enumerate(exam_data):
+        html += f"<h2>Question {index+1}</h2>"
+        html += f"<p>{question['question']}</p>"
+        for option in question['options']:
+            html += f"<label><input type='radio' name='question-{index+1}'> {option}</label><br>"
+    html += "</body></html>"
+    return html
 
 def exampapers(llm):
     chain = load_qa_chain(llm, chain_type="stuff")
+    output_parser = CommaSeparatedListOutputParser()
     uploaded_file = st.file_uploader("Upload Exam PDF file", type="pdf")
     if uploaded_file is not None:
-        
+        st.markdown(warning)
+        txt_file = pdf_to_txt(uploaded_file)
+        if txt_file is not None:
+            tpz = chain.run(input_documents=split(llm, "Key Topics", txt_file), question="What are the main topics here? Separate each topic with a ,")
+            topics = tpz.split(",")
+            numQuestions = st.number_input(
+                "Number of questions",
+                min_value=5,
+                max_value=30,
+                value=10,
+                help="Number of questions that will be generated"
+            )
+            if st.button("Generate"):
+                st.warning("Generating questions. This may take a while...")
+                try:
+                    promptt = prompt(topics, numQuestions)
+                    template = PromptTemplate(template=promptt, input_variables=[], output_parser=output_parser)
+                    llm_chain = LLMChain(prompt=template, llm=llm)
+                    res = llm_chain.predict_and_parse()
+                    # Generate HTML content here
+                    html_content = generate_html(res)
+                    # Generate PDF file
+                    output_file = "exam_paper.pdf"
+                    if generate_pdf(html_content, output_file):
+                        st.success("Questions generated successfully!")
+                        st.download_button("Download Exam Paper", output_file, "Click here to download the exam paper")
+                    else:
+                        st.error("Error occurred during PDF generation. Please try again.")
+
+                except Exception as e:
+                    st.error("Error occurred during question generation. Please try again.")
--- SmartyPants/toolkit.py
+++ SmartyPants/toolkit.py
@@ -1,33 +1,44 @@
 import PyPDF2
+import pdfkit
 import os
+import subprocess
+from typing import List
 from langchain.document_loaders import TextLoader
 from langchain.vectorstores import FAISS
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
+from typing import List
+from dataclasses import dataclass
+
+@dataclass
+class Question:
+    num: int
+    question: str
+    options: List[str]
+    answer: int
 
 db_path = '/Users/leeyilin/LifeHack-2023/SmartyPants/db'
+
 # Convert pdf file to txt file for easier processing
 def pdf_to_txt(pdf):
     pdf_filename = pdf.name  
     txt_filename = pdf_filename + '.txt'
     txt_path = '/Users/leeyilin/LifeHack-2023/SmartyPants/db/' + txt_filename
-
     with open(os.path.join(db_path, pdf_filename), 'wb') as pdf_file:
         pdf_file.write(pdf.read())
-
     with open(os.path.join(db_path, pdf_filename), 'rb') as pdf_file:
         reader = PyPDF2.PdfReader(pdf_file)
         text = ""
         for page_num in range(len(reader.pages)):
             page = reader.pages[page_num]
             text += page.extract_text()
-
     with open(txt_path, 'w', encoding='utf-8') as txt_file:
         txt_file.write(text)
     loader = TextLoader(txt_path)
     documents = loader.load()
     return documents
 
+# Prevent exceeding the token limit of OpenAI API 
 def split(query, documents):
     embeddings = OpenAIEmbeddings()
     text_splitter = CharacterTextSplitter(        
@@ -38,3 +49,21 @@ def split(query, documents):
     splitDocs = text_splitter.split_documents(documents)
     db = FAISS.from_documents(splitDocs, embeddings)
     return db.similarity_search(query)
+
+# Convert list of question to pdf 
+def generate_pdf(html_content, output_file):
+    try:
+        pdfkit.from_string(html_content, output_file)
+        return True
+    except Exception as e:
+        print(f"Error occurred during PDF generation: {str(e)}")
+        return False
+
+def prompt(topics, num):
+    return (
+        f"Create an exam of multiple choice questions with {num} "
+        f"questions and 1 possible and correct answer in each question. "
+        f"Put the correct answer in bold (surrounded by **) in its original spot. "
+        f"The exam should be about {topics}. Only generate the questions and "
+        f"answers, not the exam itself."
+    )
